db4ec0565b8dce2ea4bce3dc9df677622327b940
nick lyons <nickname@nicks-MacBook-Air.local>
2025-02-12 21:37:10 -0500

    Started article-2 animations, researched split text code

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollTrigger, SplitText);
   /**
    * Article 1
    */
@@ -55,4 +55,57 @@ document.addEventListener("DOMContentLoaded", () => {
       },
     }
   );
+
+  /**
+   * Article 2
+   */
+
+  const articleTwo = document.querySelector(".article-2");
+  const articleTwoHeader = articleTwo.querySelector(".article-2__header h2");
+  const articleTwoParagraphs = articleTwo.querySelectorAll(
+    ".article-2__header p"
+  );
+
+  let articleTwoTimeline = gsap.timeline({});
+
+  articleTwoTimeline.fromTo(
+    articleTwoHeader,
+    {
+      opacity: 0,
+    },
+    {
+      opacity: 1,
+      duration: 1,
+      ease: "power2.inOut",
+      scrollTrigger: {
+        trigger: articleTwoHeader,
+        start: "top 90%",
+        end: "+=100%",
+        scrub: true,
+        toggleActions: "play complete none none",
+      },
+    }
+  );
+
+  articleTwoParagraphs.forEach((paragraph) => {
+    articleTwoTimeline.fromTo(
+      paragraph,
+      {
+        opacity: 0,
+        y: 100,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.5,
+        ease: "power2.inOut",
+        scrollTrigger: {
+          trigger: paragraph,
+          start: "top 100%",
+          end: "+=100%",
+          scrub: true,
+        },
+      }
+    );
+  });
 });
