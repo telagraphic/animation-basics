5222601796111666205aeae2db1cc4a0f8518fc2
Nick Lyons <nick-name@Nicks-Mac-mini.local>
2025-02-16 11:14:10 -0500

    Setup paragraph text animations

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -4,6 +4,7 @@
  * The text line-height and font-size need to be precise when using overflow hidden to remove extra content space
  * This is due to font ascenders and descenders
  *
+ *
  */
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -11,165 +12,192 @@ document.addEventListener("DOMContentLoaded", () => {
 
   /*
    * Article 1
-   * Basic y fade in with overflow hidden
+   *
    */
 
-  const articleOneHeader = document.querySelector(".section-1 h1");
+  // const articleOneParagraph = document.querySelector(".section-1 p");
+  const articleOneParagraph = document.querySelector(
+    ".section-1__content-text"
+  );
 
-  const articleOneHeaderSplitText = new SplitText(articleOneHeader, {
-    type: "lines,words,chars",
-    charsClass: "overflow-hidden",
-    linesClass: "overflow-hidden", // remove if want to no clipping bottom reveal
-  });
-
-  gsap.from(articleOneHeaderSplitText.chars, {
-    yPercent: 100,
-    stagger: 0.04,
-    opacity: 0,
-    ease: "expo.out",
-  });
-
-  const articleTwoHeader = document.querySelector(".section-2 h1");
-
-  const articleTwoHeaderSplitText = new SplitText(articleTwoHeader, {
+  const articleOneParagraphSplitText = new SplitText(articleOneParagraph, {
     type: "lines,words",
-    wordsClass: "overflow-hidden",
-    linesClass: "overflow-hidden", // sets the overflow on the line
+    linesClass: "overflow-hidden",
   });
 
-  gsap.from(articleTwoHeaderSplitText.words, {
+  gsap.from(articleOneParagraphSplitText.words, {
     yPercent: 100,
+    duration: 1,
+    ease: "power2.inOut",
+    stagger: 0.01,
+    scrollTrigger: {
+      trigger: articleOneParagraph,
+      start: "top 70%",
+      end: "bottom top",
+    },
+  });
+
+  /**
+   * Article 2
+   * This will not work because the lines have the overflow-hidden applied to the line
+   * The overflow-hidden moves with the line. Need to select the words to animate instead, see Article 3
+   */
+
+  const articleTwoParagraph = document.querySelector(
+    ".section-2__content-text"
+  );
+
+  const articleTwoParagraphSplitText = new SplitText(articleTwoParagraph, {
+    type: "lines",
+    linesClass: "line",
+  });
+
+  // Add a wrapper div around each line
+  // articleTwoParagraphSplitText.lines.forEach((line) => {
+  //   const wrapper = document.createElement("div");
+  //   wrapper.classList.add("overflow-hidden");
+  //   line.parentNode.insertBefore(wrapper, line);
+  //   wrapper.appendChild(line);
+  // });
+
+  gsap.fromTo(
+    articleTwoParagraphSplitText.lines,
+    {
+      color: "#bd93f9",
+      y: 100,
+    },
+    {
+      y: 0,
+      duration: 1,
+      ease: "power2.inOut",
+      stagger: 0.01,
+      color: "#ffffff",
+      scrollTrigger: {
+        trigger: articleTwoParagraph,
+        start: "top 100%",
+        end: "bottom 20%",
+        scrub: true,
+      },
+    }
+  );
+
+  /**
+   * Article 3
+   * Apply the overflow-hidden to lines and animate the words
+   *
+   */
+
+  const articleThreeParagraph = document.querySelector(
+    ".section-3__content-text"
+  );
+
+  const articleThreeParagraphSplitText = new SplitText(articleThreeParagraph, {
+    type: "lines,words",
+    linesClass: "overflow-hidden",
+  });
+
+  let articleThreeTimeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: articleThreeParagraph,
+      start: "top 100%",
+      end: "bottom 20%",
+      scrub: true,
+    },
+  });
+
+  articleThreeParagraphSplitText.lines.forEach((line) => {
+    let lineWords = gsap.utils.selector(line);
+
+    articleThreeTimeline.from(lineWords("*"), {
+      yPercent: 100,
+      duration: 1,
+      ease: "power2.inOut",
+    });
+  });
+
+  /*
+   * Article 4
+   * Apply the overflow-hidden to lines and animate the words
+   */
+
+  const articleFourParagraph = document.querySelector(
+    ".section-4__content-text"
+  );
+
+  const articleFourParagraphSplitText = new SplitText(articleFourParagraph, {
+    type: "lines,words",
+    linesClass: "overflow-hidden",
+  });
+
+  let articleFourTimeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: articleFourParagraph,
+      start: "top 100%",
+      end: "bottom 20%",
+      scrub: true,
+    },
+  });
+
+  // Add a wrapper div around each line
+  articleFourParagraphSplitText.lines.forEach((line) => {
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("overflow-hidden");
+    line.parentNode.insertBefore(wrapper, line);
+    wrapper.appendChild(line);
+  });
+
+  gsap.fromTo(
+    articleFourParagraphSplitText.lines,
+    {
+      y: 100,
+    },
+    {
+      y: 0,
+      duration: 1,
+      ease: "power2.inOut",
+      // reveal all lines at once or staggered
+      stagger: 0.1,
+      color: "#ffffff",
+      scrollTrigger: {
+        trigger: articleFourParagraph,
+        start: "top 100%",
+        end: "top 20%",
+        scrub: true,
+      },
+    }
+  );
+
+  /*
+   * Article 5
+   * Apply split text twice to achieve the same effect as article 4
+   * https://codepen.io/PointC/pen/pxBMvy
+   */
+
+  const articleFiveParagraph = document.querySelector(
+    ".section-5__content-text"
+  );
+
+  const articleFiveParagraphChildLines = new SplitText(articleFiveParagraph, {
+    type: "lines",
+    linesClass: "linesChild",
+  });
+
+  const articleFiveParagraphParentLines = new SplitText(articleFiveParagraph, {
+    type: "lines",
+    linesClass: "linesParent",
+  });
+
+  gsap.from(articleFiveParagraphChildLines.lines, {
+    y: 100,
+    duration: 1,
+    ease: "power2.inOut",
     stagger: 0.1,
-    opacity: 0,
-    ease: "expo.out",
+    color: "#ffffff",
     scrollTrigger: {
-      trigger: articleTwoHeader,
-      start: "top 70%",
-      end: "bottom center",
+      trigger: articleFiveParagraph,
+      start: "top 100%",
+      end: "top 30%",
       scrub: true,
-    },
-  });
-
-  const articleThreeHeader = document.querySelector(".section-3 h1");
-
-  const articleThreeHeaderSplitText = new SplitText(articleThreeHeader, {
-    type: "lines,words,chars",
-    charsClass: "overflow-hidden",
-    linesClass: "overflow-hidden",
-  });
-
-  gsap.from(articleThreeHeaderSplitText.chars, {
-    xPercent: 100,
-    stagger: 0.04,
-    opacity: 0,
-    ease: "expo.out",
-    scrollTrigger: {
-      trigger: articleThreeHeader,
-      start: "top 70%",
-      end: "bottom center",
-      scrub: true,
-    },
-  });
-
-  const articleFourHeader = document.querySelector(".section-4 h1");
-  const articleFourHeaderSplitText = new SplitText(articleFourHeader, {
-    type: "lines,words,chars",
-    charsClass: "overflow-hidden",
-    linesClass: "overflow-hidden",
-  });
-
-  gsap.from(articleFourHeaderSplitText.chars, {
-    rotateY: 90,
-    xPercent: -80,
-    opacity: 0,
-    stagger: 0.04,
-    ease: "expo.out",
-    scrollTrigger: {
-      trigger: articleFourHeader,
-      start: "top 70%",
-      end: "bottom center",
-      scrub: true,
-    },
-  });
-
-  const articleFiveHeader = document.querySelector(".section-5 h1");
-  const articleFiveHeaderSplitText = new SplitText(articleFiveHeader, {
-    type: "lines,words,chars",
-    charsClass: "overflow-hidden",
-    linesClass: "overflow-hidden",
-  });
-
-  gsap.from(articleFiveHeaderSplitText.chars, {
-    rotateY: 90,
-    xPercent: -80,
-    scale: 0.2,
-    opacity: 0,
-    stagger: 0.04,
-    ease: "expo.out",
-    scrollTrigger: {
-      trigger: articleFiveHeader,
-      start: "top 70%",
-      end: "bottom center",
-      scrub: true,
-    },
-  });
-
-  /*
-   * Article 6
-   * Replaced p with a div for proper line separation
-   */
-
-  const articleSixParagraph = document.querySelector(
-    ".section-6__content-text"
-  );
-
-  const articleSixParagraphSplitText = new SplitText(articleSixParagraph, {
-    type: "lines",
-    linesClass: "lines-hidden",
-  });
-
-  gsap.from(articleSixParagraphSplitText.lines, {
-    y: 50,
-    stagger: 0.5,
-    opacity: 0,
-    ease: "expo.out",
-    scrollTrigger: {
-      trigger: articleSixParagraph,
-      start: "top 80%",
-      end: "bottom center",
-      scrub: true,
-    },
-  });
-
-  /*
-   * Article 7
-   * Fade in with overflow hidden clip not consistent for each div
-   * Overflow hidden is adding extra space to the line box! why?
-   */
-
-  const articleSevenParagraph = document.querySelector(
-    ".section-7__content-text"
-  );
-
-  const articleSevenParagraphSplitText = new SplitText(articleSevenParagraph, {
-    type: "lines",
-    linesClass: "lines-hidden",
-  });
-
-  const articleSevenParagraphLines = articleSevenParagraphSplitText.lines;
-
-  articleSevenParagraphLines.forEach((line) => {
-    line.style.overflow = "hidden";
-  });
-
-  gsap.from(articleSevenParagraphLines, {
-    yPercent: 100,
-    stagger: 0.5,
-    ease: "expo.out",
-    scrollTrigger: {
-      trigger: articleSevenParagraph,
-      start: "top 80%",
-      end: "bottom center",
     },
   });
 });
