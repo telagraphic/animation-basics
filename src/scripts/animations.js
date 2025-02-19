4ed5a8a3c04d647bb7aa23f800a75919f9e07c88
Nick Lyons <nick-name@Nicks-Mac-mini.local>
2025-02-18 20:38:13 -0500

    Basic parallax setup

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -1,3 +1,23 @@
+/**
+ * Parallax Stream
+ * 
+ * 1. Setup grid styles
+ * 2. Setup image container and img styles
+ * 3. Setup animations
+ * 
+ * Smooth scroll images
+ *  - https://codepen.io/GreenSock/pen/JjmLLWZ
+ *  - https://codepen.io/GreenSock/pen/MWVVvWz
+ *  - https://codepen.io/GreenSock/pen/PoOpobM
+ *  - https://codepen.io/GreenSock/pen/xxXadQJ
+ *  - https://codepen.io/GreenSock/pen/bGaWjpw
+ * 
+ * Smooth scroll text
+ * 
+ * 
+ */
+
+
 document.addEventListener("DOMContentLoaded", () => {
   gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
