6a77e05edf0290b135dfdf6f1e24f7753e9b1e6c
nick lyons <nickname@nicks-MacBook-Air.local>
2025-02-21 09:01:39 -0500

    Basic overflow image grid

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -22,28 +22,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-  // const smoother = ScrollSmoother.create({
-  //   smooth: 2,
-  //   effects: true,
-  // });
+  const smoother = ScrollSmoother.create({
+    smooth: 2,
+    effects: true,
+  });
 
   // speed enables the parallaxing effect
-  // smoother.effects("#smooth-content img", { speed: 0.5, lag: (index) => index * 0.1 });
+  smoother.effects("#smooth-content img", { speed: 0.5, lag: (index) => index * 0.1 });
 
-  let images = gsap.utils.toArray(".parallax-stream img");
+  // let images = gsap.utils.toArray(".parallax-stream img");
 
-  images.forEach((image) => {
-    gsap.to(image, {
-      y: 0,
-      duration: .5,
-      // opacity: 0,
-      scrollTrigger: {
-        trigger: image,
-        start: "top 50%",
-        end: "top bottom",
-        scrub: true,
-      },
-    });
-  });
+  // images.forEach((image) => {
+  //   gsap.to(image, {
+  //     y: 0,
+  //     duration: .5,
+  //     // opacity: 0,
+  //     scrollTrigger: {
+  //       trigger: image,
+  //       start: "top 50%",
+  //       end: "top bottom",
+  //       scrub: true,
+  //     },
+  //   });
+  // });
 
 });
