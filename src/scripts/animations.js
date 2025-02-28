34b9c5bdcf0bdb795277a7962b517e6e5dcabf8a
nick lyons <nickname@nicks-MacBook-Air.local>
2025-02-27 20:22:06 -0500

    CSS only marquee

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -1,10 +1,10 @@
 /**
  * Marquee by CSS and GSAP
  * 
- * 1. Why does code not work perfectly from course?
- * 2. Fix and understand
+ * 1. Why does code not work perfectly from course, Fix and understand!
+ * 2. Alernative css only solutions
  * 3. Use GSAP and horizontal loop function
- * 4. Use reverse loop from a tut below
+ * 4. Effects with scroll trigger and scroll direction
  * 
  * 
  * - look into the horizontal loop function
@@ -14,9 +14,8 @@
  * - https://ryanmulligan.dev/blog/css-marquee/
  * - https://tympanus.net/codrops/2020/03/31/css-only-marquee-effect/
  * - https://www.youtube.com/watch?v=zYBy0V91Bak
- * 
- * 
- * 
+ * - https://www.frontend.fyi/tutorials/css-only-text-marquee
+ * - https://www.youtube.com/watch?v=uw5jVO1eNF8
  */
 
 
@@ -24,26 +23,6 @@ document.addEventListener("DOMContentLoaded", () => {
   gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
   // // adding smooth scroll un-syncs the reveal clip animation
-  const smoother = ScrollSmoother.create({ smooth: 2 });
 
-  // // speed enables the parallaxing effect
-  // smoother.effects("#smooth-content img", { speed: 0.5 });
-
-
-  let timeline = gsap.timeline({ repeat: -1 })
-    .to(".marquee-gsap .marquee-text", { xPercent: -50, duration: 20, ease: "linear" });
-
-  ScrollTrigger.create({
-    trigger: ".marquee-gsap .marquee-text",
-    start: "top 100%",
-    end: "bottom top",
-    onUpdate: (self) => {
-      let velocity = self.getVelocity() * 0.01;
-      if (velocity < 0.01) {
-        velocity = velocity * -1;
-      }
-
-      timeline.timeScale(1 + velocity);
-    },
-  });
 });
+
